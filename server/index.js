3940469b2b8c1d6465ce6b1597ce3a676e501ca0
Uncle D <diegofumero2@gmail.com>
2023-05-24 14:45:24 +0200

    Followed video's instructions

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0409c728633078767eb8f232687fd8aeb5f8f0b1bbeb779c17ce3d51fd7a763375dce65d072555552da24ab3b2ff49a92f8b56e48ce36ce0baf95c593cad8173c7": 100,
+  "04fbfed86ba25f2c2714d84a0501c2713275a61999f08483dd44eee132cebb4b4a6f056f059ae94c2671d4c0d535b171b80e7e1e40f91f18015912726575652ae0": 50,
+  "046d8bb863c71ea3f0f171ed1dbaad16b02ae262cc5c04fff9b4bd0f2dba8ce7b704c70b1cfe24557a8fefd5c8a7326c6f57d27da09aaac0c39e356a79463f15b3": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
